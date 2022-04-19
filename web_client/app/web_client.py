27c9409d546bb3e349804056770cff37b70fae53
Adam Gartee <agartee@gmail.com>
2022-04-19 08:08:26 -0400

    added web client

---
+++ web_client/app/web_client.py
@@ -0,0 +1,17 @@
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+def main():
+    session = requests.Session()
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[400])
+    adapter = HTTPAdapter(max_retries=retries)
+    session.mount('http://', adapter)
+
+    response = session.get("http://localhost:8000")
+    response.raise_for_status()
+    
+    print(response.content.decode())
+
+
+if __name__ == "__main__":
+    main()
