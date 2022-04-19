593cf34e15e2740a81f98f5db2be8dfb608d7dbb
Adam Gartee <agartee@gmail.com>
2022-04-19 07:55:27 -0400

    api functional

---
+++ web_api/app/web_api.py
@@ -0,0 +1,49 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    counterFileName = f"{get_root_app_dir()}/data/counter.txt"
+    counterData = readFile(counterFileName)
+    
+    counter = 0
+    if(counterData != ""):
+        counter = int(counterData)
+
+    counter = counter + 1
+    writeFile(counterFileName, str(counter))
+        
+    if(counter % 2) == 0:
+        return f"Hello! ({counter})"
+    else:
+        return f"Hi! ({counter})"
+
+
+def readFile(fileName:str):
+    if not os.path.exists(fileName):
+        return ""
+
+    file = open(f"{fileName}", "r")
+    data = file.read()
+    file.close()
+    return data
+
+
+def writeFile(fileName:str, content:str):
+    os.makedirs(os.path.dirname(fileName), exist_ok=True)
+    file = open(f"{fileName}", "w")
+    file.write(content)
+    file.close()
+
+
+def get_root_app_dir():
+    scriptDir = os.path.dirname(os.path.abspath(__file__))
+    parentDir = os.path.abspath(os.path.join(scriptDir, os.pardir))
+    return parentDir
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000, debug=True)
